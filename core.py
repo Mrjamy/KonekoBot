6354617d7536d06703c8312f401e17b1d726059a
Jamy Muilwijk <jamy0184@gmail.com>
2018-10-20 12:47:49 +0200

    Updated extension to the full path

--- core.py
+++ core.py
@@ -3,11 +3,12 @@ from discord.ext import commands
 import config
 
 prefix = "!"
-extension_folder = "modules"
-startup_extensions = ["general"]
+startup_extensions = [
+    "modules.general",
+    "modules.response.response"
+]
 
 bot = commands.Bot(command_prefix=prefix)
-bot.case_insensitive = False
 bot.pm_help = False
 
 
@@ -19,5 +20,5 @@ async def on_ready():
 
 if __name__ == '__main__':
     for extension in startup_extensions:
-        bot.load_extension(extension_folder + "." + extension)
+        bot.load_extension(extension)
     bot.run(config.token)
