9f80e7b1d87e0ef13563ba2e7c9b665a6765109c
Jamy Muilwijk <jamy0184@gmail.com>
2018-10-25 00:56:22 +0200

    Fixed welcome/goodbye commands

--- modules/welcome.py
+++ modules/welcome.py
@@ -8,9 +8,10 @@ class Welcome:
     # Function called after member joins.
     @core.event
     async def on_member_join(self, member):
-        server = member.server.default_channel
-        fmt = 'Welcome to the {1.name} Discord server, {0.mention}, enjoy your stay.'
-        await self.bot.send_message(server, fmt.format(member, member.server))
+        guild = member.guild
+        msg = 'Welcome to the {1.name} Discord server, {0.mention}, enjoy your stay.'
+        if guild.system_channel is not None:
+            await guild.system_channel.send(msg.format(member, member.guild))
 
 
 def setup(bot):
