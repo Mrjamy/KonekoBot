2534ede7a4a638e6e3aca5be2b4ba0187d40a825
Jamy Muilwijk <16591176+jmuilwijk@users.noreply.github.com>
2018-11-02 18:00:21 +0100

    Create level.py

---
+++ modules/general/level.py
@@ -0,0 +1,19 @@
+# TODO: import sql.
+
+# TODO: open connection.
+
+# TODO: get user current xp.
+
+# TODO: get last message send by user (time).
+
+# TODO: add xp to retreived.
+
+# TODO: store xp
+
+# TODO: update last timestamp.
+
+# TODO: add cards for display.
+
+# TODO: Command - !xp (get user's card.)
+
+# TODO: Command - !levels (get the server's scoreboard.)
