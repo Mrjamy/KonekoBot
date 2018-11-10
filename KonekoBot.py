fa9d43a9250e84982ad6146acbc2aa5fecbebf55
jmuilwijk <jamy0184@gmail.com>
2018-11-10 15:53:32 +0100

    Updated logging level

--- KonekoBot.py
+++ KonekoBot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from sys import argv
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.ERROR)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
@@ -21,6 +21,7 @@ startup_extensions = [
     # "help.help",
     "music.music",
     # "nsfw.nsfw",
+    # "utility.prefix",
     # "utility.stats",
 ]
 
