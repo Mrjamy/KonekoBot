28e33c0fe4b22c5084811f8e202f42ce00148559
jmuilwijk <jamy0184@gmail.com>
2018-11-10 15:44:33 +0100

    Added logging for KonekoBot

--- KonekoBot.py
+++ KonekoBot.py
@@ -1,6 +1,13 @@
 import discord
+import logging
 from discord.ext import commands
 from sys import argv
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 prefix = "/"
 startup_extensions = [
