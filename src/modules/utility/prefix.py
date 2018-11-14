e9a5ceec392fa2002f30ddae9ccc724419e8c7cb
jmuilwijk <jamy0184@gmail.com>
2018-11-14 01:38:01 +0100

    Implemented custom prefixes
    TODO: on guild level instead of bot level.

--- src/modules/utility/prefix.py
+++ src/modules/utility/prefix.py
@@ -1,3 +1,26 @@
-# TODO: add option to customize prefix.
+# TODO: add option to customize prefix pre guild.
 
-# TODO: store guild prefix in database for later uses and reboots.
+import src.core.checks as check
+from discord.ext import commands
+from KonekoBot import KonekoBot
+
+
+class Utility:
+    """Utility commands."""
+
+    __slots__ = 'bot'
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @check.is_owner()
+    @commands.command(pass_context=True, alliases=["sp"])
+    async def set_prefix(self, ctx, prefix: str):
+        """Set the bots prefix."""
+        KonekoBot.command_prefix = commands.when_mentioned_or(prefix)
+
+        await ctx.channel.send(f"My prefix has been updated to {prefix}")
+
+
+def setup(bot):
+    bot.add_cog(Utility(bot))
