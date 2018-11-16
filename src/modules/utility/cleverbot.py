fc61cfc2b4c7c7715449fdb52700bde01426fd86
jmuilwijk <jamy0184@gmail.com>
2018-11-16 22:07:51 +0100

    Updated cleverbot draft

--- src/modules/utility/cleverbot.py
+++ src/modules/utility/cleverbot.py
@@ -1,1 +1,28 @@
 # TODO: add implementation of cleverbot
+
+from discord.ext import commands
+
+
+class CleverBot:
+    """Cog description."""
+
+    __slots__ = 'bot'
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group(pass_context=True)
+    async def clever_bot(self, ctx):
+
+        if ctx.invoked_subcommand is None:
+            await ctx.channel.send("pass an action!")
+
+    @clever_bot.command(name="start", pass_context=True)
+    async def start_session(self, ctx):
+        """Command description."""
+
+        await ctx.channel.send("initiating cleverbot session")
+
+
+def setup(bot):
+    bot.add_cog(CleverBot(bot))
