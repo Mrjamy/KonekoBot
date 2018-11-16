0f2e254fe626931f349b9d30558de6cca40a8285
jmuilwijk <jamy0184@gmail.com>
2018-11-16 22:07:34 +0100

    Toggling for commands has been scrapped

--- src/modules/utility/CommandToggle.py
+++
@@ -1,6 +0,0 @@
-# TODO: add option to load/unload cogs or commands
-
-# TODO: store guild cogs/commands in database for later uses and reboots.
-
-# @commands.command(name='play', aliases=['sing'])
-# async def toggle_cog
