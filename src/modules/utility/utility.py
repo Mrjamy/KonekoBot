50e99fba3c0f3b1d41986577cfb2e409140d6f42
jmuilwijk <jamy0184@gmail.com>
2018-11-17 03:09:02 +0100

    changed uptime command to display general stats

--- src/modules/utility/utility.py
+++ src/modules/utility/utility.py
@@ -1,4 +1,5 @@
 import time
+import discord
 import src.core.checks as check
 
 from datetime import datetime, timedelta
@@ -21,15 +22,22 @@ class Utility:
         await ctx.channel.send(f"Don't kill me!")
 
     @commands.command(pass_context=True)
-    async def uptime(self, ctx):
-        """Get the bots uptime."""
+    async def stats(self, ctx):
+        """Returns current statistics of the bot."""
 
         seconds = round(time.time() - self.bot.uptime)
 
         sec = timedelta(seconds=seconds)
         d = datetime(1, 1, 1) + sec
 
-        await ctx.channel.send(f"I have been running for {d.day-1:d}d {d.hour}h {d.minute}m {d.second}s")
+        uptime = f"{d.day-1:d}d {d.hour}h {d.minute}m {d.second}s"
+        guilds = str(len(self.bot.guilds))
+
+        embed = discord.Embed(title="Koneko's Statistics", description="", color=0x00ff00)
+        embed.add_field(name="Uptime", value=uptime, inline=True)
+        embed.add_field(name="Guilds", value=guilds, inline=True)
+
+        await ctx.channel.send(embed=embed)
 
 
 def setup(bot):
