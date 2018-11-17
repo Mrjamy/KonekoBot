8d5f5cc4853c18a1710224116d8adb1dee27666c
jmuilwijk <jamy0184@gmail.com>
2018-11-18 00:40:25 +0100

    Moved error handling

--- KonekoBot.py
+++ KonekoBot.py
@@ -5,8 +5,6 @@ import logging
 from discord.ext import commands
 from src.core.config import Settings
 import asyncio
-import sys
-import traceback
 import time
 
 logger = logging.getLogger('discord')
@@ -32,7 +30,7 @@ toggle_extensions = [
 ]
 
 core_extensions = [
-    # "src.modules.utility.CommandToggle",
+    "src.core.ErrorHandler",
 ]
 
 
@@ -71,63 +69,6 @@ def initialize(bot_class=KonekoBot):
         # Bot logged in.
         print(f'Logged in as {bot.user}')
         print(f'I am in {len(bot.guilds)} guilds.')
-
-    @bot.event
-    async def on_command_error(ctx, error):
-        """The event triggered when an error is raised while invoking a command.
-        ctx   : Context
-        error : Exception"""
-
-        # This prevents any commands with local handlers being handled here in on_command_error.
-        if hasattr(ctx.command, 'on_error'):
-            return
-
-        ignored = (commands.CommandNotFound, commands.UserInputError)
-
-        # Allows us to check for original exceptions raised and sent to CommandInvokeError.
-        # If nothing is found. We keep the exception passed to on_command_error.
-        error = getattr(error, 'original', error)
-
-        # Anything in ignored will return and prevent anything happening.
-        if isinstance(error, ignored):
-            return
-
-        elif isinstance(error, commands.DisabledCommand):
-            await ctx.send(f'{bot.settings.prefix}{ctx.command} has been disabled.')
-            return
-
-        elif isinstance(error, commands.NoPrivateMessage):
-            try:
-                await ctx.channel.send(f'{bot.settings.prefix}{ctx.command} can not be used in Private Messages.')
-            except:
-                pass
-            return
-
-        elif isinstance(error, commands.BadArgument):
-            await ctx.send(f'Refer to.{bot.settings.prefix}help {ctx.command}')
-            return
-
-        elif isinstance(error, commands.BotMissingPermissions):
-            missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
-            if len(missing) > 2:
-                fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
-            else:
-                fmt = ' and '.join(missing)
-            await ctx.send(f'I need the **{fmt}** permission(s) to run this command.')
-            return
-
-        if isinstance(error, commands.MissingPermissions):
-            missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
-            if len(missing) > 2:
-                fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
-            else:
-                fmt = ' and '.join(missing)
-            await ctx.send(f'You need the **{fmt}** permission(s) to use this command.')
-            return
-
-        # All other Errors not returned come here... And we can just print the default TraceBack.
-        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
     return bot
 
