f24ead2cff04fb3c808ff598a8b364d08c7a1e43
jmuilwijk <jamy0184@gmail.com>
2018-11-17 02:34:24 +0100

    Added /uptime command

--- src/modules/utility/utility.py
+++ src/modules/utility/utility.py
@@ -1,4 +1,7 @@
+import time
 import src.core.checks as check
+
+from datetime import datetime, timedelta
 from discord.ext import commands
 
 
@@ -16,6 +19,17 @@ class Utility:
         """Shuts the bot down."""
 
         await ctx.channel.send(f"Don't kill me!")
+
+    @commands.command(pass_context=True)
+    async def uptime(self, ctx):
+        """Get the bots uptime."""
+
+        seconds = round(time.time() - self.bot.uptime)
+
+        sec = timedelta(seconds=seconds)
+        d = datetime(1, 1, 1) + sec
+
+        await ctx.channel.send(f"I have been running for {d.day-1:d}d {d.hour}h {d.minute}m {d.second}s")
 
 
 def setup(bot):
