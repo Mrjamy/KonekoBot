757a62dea0ae486044d8056a7bc6a459468e0430
jmuilwijk <jamy0184@gmail.com>
2018-11-29 01:25:28 +0100

    added support to supplie amount of die to roll

--- src/modules/games/dnd.py
+++ src/modules/games/dnd.py
@@ -1,5 +1,7 @@
-from discord.ext import commands
+import discord
 import random
+from discord.ext import commands
+import textwrap
 
 # TODO: add a dungeon/dnd based game.
 
@@ -19,11 +21,23 @@ class Dungeon:
     def __init__(self, bot):
         self.bot = bot
 
+    # TODO: update the /roll command to /roll <dices>d<max_roll>
     @commands.command(pass_context=True)
-    async def roll(self, ctx, max_roll: int = 6):
+    async def roll(self, ctx, max_roll: int = 6, die: int = 1):
         """Rolls a dice."""
-        roll = random.randint(1, max_roll)
-        await ctx.channel.send("Rolled " + str(roll))
+
+        embed = discord.Embed(title=f'{ctx.author} rolled {die} Dice - {max_roll}',
+                              color=discord.Color.dark_purple())
+        rolls = ''
+        total = 0
+        for x in range(0, die):
+            roll = random.randint(1, max_roll)
+            total += roll
+            rolls += f'{str(roll)} '
+
+        embed.add_field(name='Rolls', value=rolls, inline=False)
+        embed.add_field(name='Total', value=total, inline=False)
+        await ctx.channel.send(embed=embed)
 
 
 def setup(bot):
