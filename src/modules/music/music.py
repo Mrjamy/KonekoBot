6b2a432b57b085810259ba787e53eb1202ec0a60
jmuilwijk <jamy0184@gmail.com>
2018-12-05 20:57:00 +0100

    added requirement for dj role

--- src/modules/music/music.py
+++ src/modules/music/music.py
@@ -249,6 +249,7 @@ class Music:
 
         await player.queue.put(source)
 
+    @checks.is_dj()
     @commands.guild_only()
     @commands.command(name='pause', pass_context=True)
     async def pause_(self, ctx):
@@ -263,6 +264,7 @@ class Music:
         vc.pause()
         await ctx.send(f'**`{ctx.author}`**: Paused the song!')
 
+    @checks.is_dj()
     @commands.guild_only()
     @commands.command(name='resume', pass_context=True)
     async def resume_(self, ctx):
