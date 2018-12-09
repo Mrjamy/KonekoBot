32067e0a1a64f2ff337d9e437be7532429bebd53
jmuilwijk <jamy0184@gmail.com>
2018-12-09 19:16:25 +0100

    add currency module

---
+++ src/modules/economy/currency.py
@@ -0,0 +1,14 @@
+# TODO: add currency commands.
+
+# TODO: add command /login
+
+# TODO: add command /give <user> <amount> (dev ^ only)
+# TODO: add command /take <user> <amount> (dev ^ only)
+
+# TODO: add command /transfer <user> <amount>
+
+# TODO: add command /coins
+
+# TODO: give the currency a name.
+
+# TODO: add commands to bet. where different games have different odds and different payouts.
