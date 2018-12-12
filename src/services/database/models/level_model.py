38b6e9053007376eb96ebc4b9a1c27054a4e3af8
jmuilwijk <jamy0184@gmail.com>
2018-12-13 00:25:48 +0100

    updated user/level storage

--- src/services/database/models/level_model.py
+++ src/services/database/models/level_model.py
@@ -1,6 +1,14 @@
-from sqlalchemy import Column
+import time
+from sqlalchemy import (
+    Column,
+    create_engine
+)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.sqlite import INTEGER, TEXT, TIMESTAMP
+
+
+db_uri = 'sqlite:///level.sqlite'
+engine = create_engine(db_uri)
 
 Base = declarative_base()
 
@@ -8,8 +16,11 @@ Base = declarative_base()
 class Level(Base):
     __tablename__ = 'levels'
     __table_args__ = {'sqlite_autoincrement': True}
-    snowflake = Column(TEXT, nullable=False)
+    snowflake = Column(TEXT, nullable=False, primary_key=True)
     guild = Column(TEXT, nullable=False)
     experience = Column(INTEGER, nullable=False)
     level = Column(INTEGER, nullable=False)
-    last_message = Column(TIMESTAMP, nullable=False)
+    last_message = Column(TIMESTAMP, nullable=False, default=time.time())
+
+
+Base.metadata.create_all(engine)
