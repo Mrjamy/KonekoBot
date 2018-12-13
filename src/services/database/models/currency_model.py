95760e10c48e76c5ff6c8c538b29085d847affc3
jmuilwijk <jamy0184@gmail.com>
2018-12-14 00:50:46 +0100

    updated level command
    implemented sqllite database and user mention

--- src/services/database/models/currency_model.py
+++ src/services/database/models/currency_model.py
@@ -15,7 +15,8 @@ Base = declarative_base()
 class Currency(Base):
     __tablename__ = 'currency'
     __table_args__ = {'sqlite_autoincrement': True}
-    snowflake = Column(INTEGER, nullable=False, primary_key=True)
+    id = Column(INTEGER, nullable=False, primary_key=True)
+    snowflake = Column(INTEGER, nullable=False)
     guild = Column(INTEGER, nullable=False)
     amount = Column(TEXT, nullable=False)
 
