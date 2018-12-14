ed6ac8cc2f7f038c7538fe681a7b6939cdcd0302
jmuilwijk <jamy0184@gmail.com>
2018-12-14 08:30:43 +0100

    updated setup

--- src/core/setup.py
+++ src/core/setup.py
@@ -1,3 +1,24 @@
-from src.services.database.models.currency_model import Currency
-from src.services.database.models.level_model import Level
+import os
 
+
+class Setup:
+    def __init__(self):
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.data = rf'{self.dir_path}/data'
+
+        self.currency = rf'{self.data}/currency.sqlite'
+        self.level = rf'{self.data}/currency.sqlite'
+
+    def setup(self):
+        self.database()
+
+    def database(self):
+        files = [self.currency, self.level]
+
+        for file in files:
+            # if not os.path.exists(file):
+            #     open(file, 'w').close()
+            try:
+                file = open(file, 'r')
+            except (IOError, FileNotFoundError):
+                file = open(file, 'w')
