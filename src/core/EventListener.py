626cfcd4259fb335063c074df12119ebad663d8c
jmuilwijk <jamy0184@gmail.com>
2019-03-23 17:36:28 +0100

    Moved events to class EventListener

---
+++ src/core/EventListener.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+
+class EventListener(commands.Cog):
+    __slots__ = 'bot'
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        """KonekoBot on_ready event."""
+        print(self)
+        game = self.bot.settings.prefix + "h for help"
+        activity = discord.Game(name=game)
+        await self.bot.change_presence(status=discord.Status.online, activity=activity)
+        # Bot logged in.
+        print(f'Logged in as {self.bot.user}')
+        print(f'I am in {len(self.bot.guilds)} guilds.')
+
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        self.bot.command_count += 1
+
+
+def setup(bot):
+    bot.add_cog(EventListener(bot))
