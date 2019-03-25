28bafe70dbd345cb544f7c49d2fe79cd763bbdb3
jmuilwijk <jamy0184@gmail.com>
2019-03-25 20:58:07 +0100

    Cleaning up

--- src/core/EventListener.py
+++ src/core/EventListener.py
@@ -11,7 +11,6 @@ class EventListener(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         """KonekoBot on_ready event."""
-        print(self)
         game = self.bot.settings.prefix + "h for help"
         activity = discord.Game(name=game)
         await self.bot.change_presence(status=discord.Status.online, activity=activity)
