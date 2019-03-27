1f88b8373720dc2f88172a691b2552e99d53a3a9
jmuilwijk <jamy0184@gmail.com>
2019-03-27 01:02:37 +0100

    Added users to stats command

--- src/cogs/utility/utility.py
+++ src/cogs/utility/utility.py
@@ -32,11 +32,13 @@ class Utility(commands.Cog):
 
         uptime = f"{d.day-1:d}d {d.hour}h {d.minute}m {d.second}s"
         guilds = str(len(self.bot.guilds))
+        members = str(len(list(self.bot.get_all_members())))
         command_count = self.bot.command_count + 1
 
         embed = discord.Embed(title="Koneko's Statistics", description="", color=discord.Color.dark_purple())
-        embed.add_field(name="Uptime", value=uptime, inline=True)
         embed.add_field(name="Guilds", value=guilds, inline=True)
+        embed.add_field(name="Users", value=members, inline=True)
+        embed.add_field(name="Uptime", value=uptime, inline=True)
         embed.add_field(name="Commands executed", value=command_count, inline=True)
 
         await ctx.channel.send(embed=embed)
