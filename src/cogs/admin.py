6b1fb3f46c7b061f23990132ee9a5fe3db818e8c
jmuilwijk <jamy0184@gmail.com>
2019-03-29 22:12:23 +0100

    Added admin command to change presence

--- src/cogs/admin.py
+++ src/cogs/admin.py
@@ -1,5 +1,7 @@
-from src.core.checks import Checks
+import discord
 from discord.ext import commands
+
+from src.core.checks import Checks
 
 
 class Admin(commands.Cog):
@@ -10,6 +12,21 @@ class Admin(commands.Cog):
     async def cog_check(self, ctx):
         return await self.bot.is_owner(ctx.author)
 
+
+    @commands.command(aliases=["watch", "listen"], pass_context=True)
+    async def game(self, ctx, *, name: str = None):
+        """Change koneko's presence, owner only"""
+
+        activities = {
+            'game': discord.Game(name=name),
+            'listen': discord.Activity(type=discord.ActivityType.listening, name=name),
+            'watch': discord.Activity(type=discord.ActivityType.watching, name=name)
+        }
+
+        activity = activities[ctx.invoked_with]
+
+        await self.bot.change_presence(status=discord.Status.online, activity=activity)
+        return
 
 def setup(bot):
     bot.add_cog(Admin(bot))
