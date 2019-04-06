48c01488b111b9d769a976bf5a69beb6387928d1
jmuilwijk <jamy0184@gmail.com>
2019-04-06 21:12:17 +0200

    Updated general cog to be more clear

--- src/helpers/random/image_provider.py
+++ src/helpers/random/image_provider.py
@@ -3,20 +3,16 @@ import os
 import random
 
 
-class ImageGenerator:
-    __slots__ = ['local_dir', 'external_dir', 'tag']
+class ImageProvider:
+    __slots__ = ['local_dir', 'external_dir']
 
-    def __init__(self, tag: str):
+    def __init__(self):
         path = os.path.join(os.path.dirname(__file__), '..', '..')
 
         self.local_dir = os.path.join(rf'{path}', 'core', 'images')
         self.external_dir = rf'https://raw.githubusercontent.com/jmuilwijk/KonekoBot/development/src/core/images/'
-        self.tag = tag
 
-    def to_image(self):
-        return self.__image(self.tag)
-
-    def __image(self, query: str):
+    def image(self, query: str):
         local_dir = os.path.join(self.local_dir, query)
         remote_dir = rf'{self.external_dir}{query}'
 
