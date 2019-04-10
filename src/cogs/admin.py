ef0c462a6ccdd16946f163a0e2f2ea3f4218df5b
jmuilwijk <jamy0184@gmail.com>
2019-04-10 21:43:26 +0200

    Added command to update koneko\'s sentences

--- src/cogs/admin.py
+++ src/cogs/admin.py
@@ -1,3 +1,6 @@
+# Builtins
+import json
+
 # Pip
 import discord
 from discord.ext import commands
@@ -28,7 +31,53 @@ class Admin(commands.Cog):
         activity = activities[ctx.invoked_with]
 
         await self.bot.change_presence(status=discord.Status.online, activity=activity)
-        return
+
+    @commands.group(hidden=True)
+    async def sentence(self, ctx):
+        if ctx.invoked_subcommand is None:
+            with open('src/cogs/utils/sentences.json') as f:
+                data = json.load(f)
+                return await ctx.channel.send(json.dumps(data, indent=4, sort_keys=True))
+
+    @sentence.command()
+    async def get(self, ctx, command : str, string : str):
+        with open('src/cogs/utils/sentences.json') as f:
+            data = json.load(f)
+            try:
+                response = data[command][string]
+                await ctx.channel.send(f"{command}.{string} is `{response}`")
+            except KeyError:
+                return await ctx.channel.send(F"Could not find {command}.{string}")
+
+    @get.error
+    async def get_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('Some parameters seem missing :thinking:')
+        else:
+            raise error
+
+    @sentence.command()
+    async def update(self, ctx, command : str, string : str, *, new : str):
+        # Open file in read mode.
+        with open('src/cogs/utils/sentences.json', 'r') as f:
+            data = json.load(f)
+
+        # Try updating the key.
+        try:
+            data[command][string] = new
+        except KeyError:
+            return await ctx.channel.send(F"Could not find {command}.{string}")
+
+        # Write the modified json object back to the file.
+        with open('src/cogs/utils/sentences.json', 'w') as f:
+            json.dump(data, f, indent=4, sort_keys=True)
+
+    @update.error
+    async def update_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send('Some parameters seem missing :thinking:')
+        else:
+            raise error
 
 def setup(bot):
     bot.add_cog(Admin(bot))
