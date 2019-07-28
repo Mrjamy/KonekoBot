a855033e7237c0671f71297ea69165820074bc78
jmuilwijk <jamy0184@gmail.com>
2019-07-28 17:31:14 +0200

    Removed music module.
    The music module needs a lot of work and refactoring, will be removed
    for now (might be added back later)

--- src/core/checks.py
+++ src/core/checks.py
@@ -1,15 +1,9 @@
 # Pip
-import discord
 from discord.ext import commands
 
 # Locals
 from src.core.config import Settings
-from src.core.exceptions import (
-    DevOnly,
-    NoPermission,
-    DjOnly,
-    NotInVoiceChannel
-)
+from src.core.exceptions import NoPermission
 
 settings = Settings()
 
@@ -23,28 +17,4 @@ class Checks:
                 return True
             else:
                 raise NoPermission
-        return commands.check(predicate)
-
-    @staticmethod
-    def is_dj():
-        async def predicate(ctx):
-            if "dj" in [role.name.lower() for role in ctx.author.roles]:
-                return True
-            elif commands.is_owner():
-                return True
-            else:
-                # TODO: v2.0 Add option to set the DJ role to any existing role.
-                # TODO: v2.0 Store this setting in a database.
-                # ctx.send("This command requires you to have the role DJ")
-                raise DjOnly
-        return commands.check(predicate)
-
-    @staticmethod
-    def is_connected_voice():
-        async def predicate(ctx):
-            if hasattr(ctx.author.voice, 'channel'):
-                return True
-            else:
-                raise NotInVoiceChannel
-
         return commands.check(predicate)
