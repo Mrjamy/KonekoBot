919f55e1fe5901714fefd3fa0307f4c6da5b68da
mrjamy <jamy0184@gmail.com>
2020-02-10 19:09:26 +0100

    Added give_item command

--- src/cogs/general.py
+++ src/cogs/general.py
@@ -32,6 +32,7 @@ class General(commands.Cog):
 
     @commands.command(aliases=["pat", "kiss", "slap", "lewd"])
     async def hug(self, ctx, users: commands.Greedy[discord.Member]):
+        """Interact with other users."""
         provider = ImageProvider()
 
         url = provider.image(query=ctx.invoked_with)
@@ -42,8 +43,10 @@ class General(commands.Cog):
         with open('src/cogs/utils/sentences.json') as f:
             data = json.load(f)
             if any(u.id in [502913609458909194, 533653653362311188] for u in users):
+                # TODO: use fstrings.
                 message = data[ctx.invoked_with]['koneko'].format(Name.nick_parser(ctx.message.author),)
             else:
+                # TODO: use fstrings.
                 message = data[ctx.invoked_with]['other'].format(Name.nick_parser(ctx.message.author), mentions)
 
         embed = discord.Embed(title=message,
@@ -52,7 +55,14 @@ class General(commands.Cog):
 
         await ctx.channel.send(embed=embed)
 
-    # TODO: v1.2 add command /beer <user>
+    @commands.command()
+    async def give_item(self, ctx, users: commands.Greedy[discord.Member], *, item):
+        if len(users) == 0:
+            users = [self.bot.user]
+        mentions = ' '.join([f'{Name.nick_parser(user)}' for user in users])
+        message = f"{ctx.message.author} gives {mentions} {item}"
+
+        await ctx.channel.send(embed=discord.Embed(title=message, color=discord.Color.dark_purple()))
 
 
 def setup(bot):
