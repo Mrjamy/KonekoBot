c1c0425c91b4f5331654d44ff4f2715682af3e92
jmuilwijk <jamy0184@gmail.com>
2020-04-16 21:56:00 +0200

    Simplified cog reloading

--- src/core/HotReload.py
+++ src/core/HotReload.py
@@ -15,26 +15,17 @@ class HotReload(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.location = "src.cogs."
 
     async def cog_check(self, ctx) -> bool:
         return await self.bot.is_owner(ctx.author)
 
     @commands.command(aliases=["load"], hidden=True)
-    async def reload(self, ctx, *cogs: str) -> coroutine:
-        for cog in cogs:
-            self.bot.reload_extension(cog)
-
-        reloaded = " ".join(f"{self.location}{cogs}")
-        return await ctx.channel.send(F"loaded {reloaded}")
+    async def reload(self, ctx, cog: str) -> coroutine:
+        return await ctx.channel.send(F"loaded {cog}")
 
     @commands.command(hidden=True)
-    async def unload(self, ctx, *cogs: str) -> coroutine:
-        for cog in cogs:
-            self.bot.unload_extension(cog)
-
-        unloaded = " ".join(f"{self.location}{cogs}")
-        return await ctx.channel.send(F"unloaded {unloaded}")
+    async def unload(self, ctx, cog: str) -> coroutine:
+        return await ctx.channel.send(F"unloaded {cog}")
 
 
 def setup(bot) -> None:
