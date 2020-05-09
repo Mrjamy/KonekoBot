093c012b0a9cc39509332a0ac8de73b6a9684a30
jmuilwijk <jamy0184@gmail.com>
2020-05-09 05:53:26 +0200

    Removed unused checks
    Error raised by check is now unused too, commands.MissingPermissions
    should be raised when the user misses permissions.
    
    Signed-off-by: jmuilwijk <jamy0184@gmail.com>

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -1,12 +1,3 @@
-# Pip
-from discord.ext import commands
-
-
-class NoPermission(commands.CommandError):
-    """No permission for the given action."""
-    pass
-
-
 class NotEnoughBalance(Exception):
     """Not enough balance for the command."""
     pass
