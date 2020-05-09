90bd88dd65709608d7fe3600846f7586a558626b
jmuilwijk <jamy0184@gmail.com>
2020-05-09 19:26:01 +0200

    Enabled module logging
    Added logging for discord and asyncio, moved koneko's logging away from
    the main file for cleanup.
    
    Signed-off-by: jmuilwijk <jamy0184@gmail.com>

---
+++ src/core/logging.py
@@ -0,0 +1,22 @@
+# Builtins
+import logging
+
+
+class Logging:
+    def __init__(self):
+        for module, level in {
+            'koneko': logging.DEBUG,
+            'discord': logging.CRITICAL,
+            'asyncio': logging.CRITICAL
+        }.items():
+            self.file_logger(module, level)
+
+    @staticmethod
+    def file_logger(module: str, level: int):
+        logger = logging.getLogger(module)
+        logger.setLevel(level)
+        logger_file_handler = logging.FileHandler(f'logs/{module}.log')
+        logger_format = '%(asctime)-15s - [%(process)-6s] %(levelname)-8s - %(name)s - %(message)s'
+        logger_formatter = logging.Formatter(logger_format)
+        logger_file_handler.setFormatter(logger_formatter)
+        logger.addHandler(logger_file_handler)
