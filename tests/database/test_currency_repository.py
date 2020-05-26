4e38b7e09838f9fea5b58dae9fd0ca618c62abb9
jmuilwijk <jamy0184@gmail.com>
2020-05-26 13:00:52 +0200

    Start to unittests
    Signed-off-by: jmuilwijk <jamy0184@gmail.com>

---
+++ tests/database/test_currency_repository.py
@@ -0,0 +1,20 @@
+import unittest
+
+
+class TestCurrencyRepository(unittest.TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_create_user(self) -> None:
+        pass
+
+    def test_delete_user(self) -> None:
+        pass
+
+    def test_mutate_balance(self) -> None:
+        pass
+
