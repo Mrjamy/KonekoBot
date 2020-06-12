41e7c3ddc502042b38b89d38787ea0874786fa54
Soufyan <MetaByte149@gmail.com>
2020-06-12 22:14:33 +0200

    Made the code more clear and simpler

--- src/cogs/games.py
+++ src/cogs/games.py
@@ -29,68 +29,59 @@ class Games(commands.Cog):
     @commands.command()
     async def rps(self, ctx, choice: str = "") -> None:
         """Play a game of rock paper scissors."""
-        options = {
-            "r": "rock",
-            "p": "paper",
-            "s": "scissors"
-        }
+    options = {
+        "r": 1,
+        "p": 2,
+        "s": 3
+    }
 
-        if choice == "":
-            m = await ctx.channel.send("Please choose from: rock, paper, scissors")
+    reverse_options = {
+        1: "rock",
+        2: "paper",
+        3: "scissor"
+    }
 
-            def validate(m_):
-                return m_.author == ctx.author and m_.channel == ctx.channel
 
-            try:
-                choice = await ctx.bot.wait_for('message', check=validate,
-                                                timeout=60)
-                choice = choice.content
-            except asyncio.TimeoutError:
-                await m.delete()
-                return
-
-        player = choice.lower()
-
-        if player in options:
-            player = options.get(player)
-        elif player not in options.values():
-            await DiscordEmbed.error(ctx, title='Please choose from: rock, '
-                                                'paper, scissors')
+    # Getting the input inside <choice>
+    if choice == "":
+        m = await ctx.channel.send("Please choose from: rock, paper, scissors")
+        def validate(m_):
+            return m_.author == ctx.author and m_.channel == ctx.channel
+        try:
+            choice = await ctx.bot.wait_for('message', check=validate,
+                                            timeout=60)
+            choice = choice.content
+        except asyncio.TimeoutError:
+            await m.delete()
             return
+    # only look at the 1st character of choice
+    player = choice.lower()[0]
+    if player in options:
+        player_choice = options.get(player)
+    elif player not in options.values():
+        await DiscordEmbed.error(ctx, title='Please choose from: rock, '
+                                            'paper, scissors')
+        return
 
-        bot = random.choice(list(options.values()))
+    # make the bot choose an option
+    bot_choice_key = random.choice(list(options))
+    bot_choice = options[bot_choice_key]
 
-        if player == bot:
-            color = discord.Color.dark_grey()
-            message = "Tie!"
-        elif player == "rock":
-            if bot == "paper":
-                color = discord.Color.red()
-                message = f"You lose! {bot} covers {player}"
-            else:
-                color = discord.Color.green()
-                message = f"You win! {player} smashes {bot}"
-        elif player == "paper":
-            if bot == "Scissors":
-                color = discord.Color.red()
-                message = f"You lose! {bot} cut {player}"
-            else:
-                color = discord.Color.green()
-                message = f"You win! {player} covers {bot}"
-        elif player == "scissors":
-            if bot == "rock":
-                color = discord.Color.red()
-                message = f"You lose! {bot} smashes {player}"
-            else:
-                color = discord.Color.green()
-                message = f"You win! {player} cut {bot}"
-        else:
-            color = discord.Color.red()
-            message = "Oops something went wrong"
-
-        await DiscordEmbed.send(ctx, title=message, color=color)
+    # checking who won and sending the appropiate message
+    res = ((player_choice - bot_choice) % 3)
+    if res == 0:
+        message = f"It is a tie! your {reverse_options[player_choice]} went even with my {reverse_options[bot_choice]}!"
+        await DiscordEmbed.message(ctx, title=message)
+    elif res == 1:
+        message = f"You won! your {reverse_options[player_choice]} beat my {reverse_options[bot_choice]}!"
+        await DiscordEmbed.confirm(ctx, title=message)
+    else:
+        message = f"You lost! your {reverse_options[player_choice]} lost to my {reverse_options[bot_choice]}!"
+        await DiscordEmbed.error(ctx, title=message)
 
 
 def setup(bot) -> None:
     """The setup function to add this cog to Koneko."""
     bot.add_cog(Games(bot))
+
+
