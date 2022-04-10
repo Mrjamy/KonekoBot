d01f3bd28fd86452fc4e0116c3a1708244cea4d0
Mrjamy <jamy0184@gmail.com>
2022-04-11 00:29:46 +0200

    Fixed typo
    Signed-off-by: Mrjamy <jamy0184@gmail.com>

--- src/cogs/ideas/alert.py
+++ src/cogs/ideas/alert.py
@@ -1,3 +1,3 @@
 # TODO: add the option to send a notification to all servers, takes input, owner only.
 
-# TODO: allow for custumizable alert channel based on server.
+# TODO: allow for customizable alert channel based on server.
